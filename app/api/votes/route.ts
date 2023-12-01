664a1d4b7f131524dc5aac57185420c04d93fbc1
Truls Henrik Jakobsen <thj@variant.no>
2023-12-01 07:41:42 +0100

    start creating backend

---
+++ app/api/votes/route.ts
@@ -0,0 +1,25 @@
+export async function GET() {
+  const votes = (await prisma?.vote.findMany()) ?? [];
+  return Response.json(votes);
+}
+
+export async function POST(request: Request) {
+  const {
+    julebrusId,
+    taste,
+    smell,
+    color,
+  }: { julebrusId: number; taste: number; smell: number; color: number } =
+    await request.json();
+
+  const createdVote = await prisma?.vote.create({
+    data: {
+      julebrusId,
+      taste,
+      smell,
+      color,
+    },
+  });
+
+  return Response.json(createdVote);
+}
