53ce425fd45aea13b7a645565dbb4848550b2c9a
Jakob Kielland <jakob-ek@hotmail.com>
2023-12-01 08:31:19 +0100

    decide that progressive enhancement is for losers

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,39 @@
+"use client";
+
 import RadioInput from "./components/RadioInput";
 
 export default function Home() {
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+
+    await fetch("/api/vote", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  };
   return (
-    <main className='flex min-h-screen flex-col p-8'>
-      <h1 className='text-4xl font-bold'>Julebrus</h1>
-      <form action='/api/vote' method='post' className='flex flex-col gap-4'>
-        <label htmlFor='soda'>
+    <main className="flex min-h-screen flex-col p-8">
+      <h1 className="text-4xl font-bold">Julebrus</h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <label htmlFor="soda">
           Brus:
           <br />
-          <select id='julebrusId' name='julebrusId'>
-            <option value='1'>Brus 1</option>
-            <option value='2'>Brus 2</option>
-            <option value='3'>Brus 3</option>
+          <select id="julebrusId" name="julebrusId">
+            <option value="1">Brus 1</option>
+            <option value="2">Brus 2</option>
+            <option value="3">Brus 3</option>
           </select>
         </label>
-        <RadioInput id='color' name='color' label='Farge' />
-        <RadioInput id='smell' name='smell' label='Lukt' />
-        <RadioInput id='taste' name='taste' label='Smak' />
-        <input type='submit' />
+        <RadioInput id="color" name="color" label="Farge" />
+        <RadioInput id="smell" name="smell" label="Lukt" />
+        <RadioInput id="taste" name="taste" label="Smak" />
+        <input type="submit" />
       </form>
     </main>
   );
